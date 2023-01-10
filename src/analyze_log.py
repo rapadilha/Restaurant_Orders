a40b53a28e50ee26ce3ee8925e662dde2bcdbad2
Raphael <raphaellfpg@gmail.com>
2023-01-10 05:53:37 -0300

    fix: FileNotFOundError

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -13,7 +13,7 @@ def read_file(path_to_file):
                 orders.append(order)
         return orders
     except FileNotFoundError:
-        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+        raise ValueError(f"Arquivo inexistente: '{path_to_file}'")
 
 
 def most_requested_by_maria(orders):
