7d02e5b5d05f0d056343a70383a78c575e3c5cb8
Raphael <raphaellfpg@gmail.com>
2023-01-10 05:44:28 -0300

    fix: linter issues

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,9 +1,10 @@
 from statistics import mode
 
+
 def read_file(path_to_file):
     if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
-    
+
     try:
         orders = []
         with open(path_to_file, 'r') as file:
@@ -13,16 +14,16 @@ def read_file(path_to_file):
         return orders
     except FileNotFoundError:
         raise f"Extensão inválida: '{path_to_file}'"
-    
-    
+
 
 def most_requested_by_maria(orders):
     mariaDishes = []
     for order in orders:
         if order[0] == 'maria':
             mariaDishes.append(order[1])
-    
+
     return mode(mariaDishes)
+
 
 def arnaldo_hamburguer(orders):
     count = 0
@@ -32,18 +33,20 @@ def arnaldo_hamburguer(orders):
 
     return count
 
+
 def joao_never_request(orders):
     joaoDishes = []
     notResquests = set()
     for order in orders:
         if order[0] == 'joao':
             joaoDishes.append(order[1])
-    
+
     for dish in orders:
         if dish[1] not in joaoDishes:
             notResquests.add(dish[1])
 
     return notResquests
+
 
 def joao_didnt_go(orders):
     joaoDays = []
@@ -51,12 +54,13 @@ def joao_didnt_go(orders):
     for order in orders:
         if order[0] == 'joao':
             joaoDays.append(order[2])
-    
+
     for dish in orders:
         if dish[2] not in joaoDays:
             didntGo.add(dish[2])
 
     return didntGo
+
 
 def write_infos(orders):
     maria = most_requested_by_maria(orders)
@@ -74,7 +78,7 @@ def write_infos(orders):
     write_file.write(result)
     write_file.close()
 
+
 def analyze_log(path_to_file):
     orders = read_file(path_to_file)
     write_infos(orders)
-    
